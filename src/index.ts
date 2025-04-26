6a0722fea3b4ec42d19bc6d8591a512cf742ba32
Maxcwt <148951047+Maxcwt@users.noreply.github.com>
2025-04-26 16:58:05 +0200

    feat: [AIC-018] add Swagger documentation setup and update profile routes with Swagger annotations

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import profileRoutes from './routes/profile.routes';
+import { setupSwagger } from "./swagger";
 
 const app = express();
 
@@ -7,11 +8,13 @@ app.use(express.json());
 app.disable("x-powered-by");
 
 app.use('/profiles', profileRoutes);
+setupSwagger(app);
 
 if (process.env.NODE_ENV !== "test") {
     const PORT = process.env.PORT || 3000;
     app.listen(PORT , () => {
         console.log(`🚀 Server running on http://localhost:${PORT}`);
+        console.log(`📚 Swagger docs on http://localhost:${PORT}/api-docs`);
     });
 }
 
