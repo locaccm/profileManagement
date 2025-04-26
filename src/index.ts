f97758852f2158a0c4132db698ab77cedde778e6
Maxcwt <148951047+Maxcwt@users.noreply.github.com>
2025-04-26 16:15:18 +0200

    fix: [AIC-018] - Fix index.ts for serveur run

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,16 @@ import profileRoutes from './routes/profile.routes';
 
 const app = express();
 
+app.use(express.json());
+app.disable("x-powered-by");
 
 app.use('/profiles', profileRoutes);
+
+if (process.env.NODE_ENV !== "test") {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT , () => {
+        console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+}
+
+export default app;
